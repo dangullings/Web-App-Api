702d4acf427ee3067652b6a142422399ca65f1a9
dangullings <dangullings@gmail.com>
2021-05-30 18:26:01 -0500

    Update LineItemRepo.java

--- src/main/java/com/example/polls/repository/LineItemRepo.java
+++ src/main/java/com/example/polls/repository/LineItemRepo.java
@@ -5,11 +5,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.example.polls.model.ClassDate;
 import com.example.polls.model.LineItem;
 
 @Repository
